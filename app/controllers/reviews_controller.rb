ab52f3bb55c3f9f9818aa33528744643e2791f0d
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-07-29 12:58:50 -0500

    commit tree a3218e2b8aa24d0a8e95e796092dd3c40aed13d5

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,12 @@ class ReviewsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    reviews = Review.all
+    if params[:dog_house_id]
+      reviews = Review.find_by dog_house_id: params[:dog_house_id]
+      
+    else
+      reviews = Review.all
+    end
     render json: reviews, include: :dog_house
   end
 
